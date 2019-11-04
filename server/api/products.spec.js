20c3e1f40ffb1a2e776155f600130b4e8da666a7
Haley Sambursky <hsambursky46@gmail.com>
2019-11-04 17:19:11 -0500

    all products #6 model, and get route defined

---
+++ server/api/products.spec.js
@@ -0,0 +1,31 @@
+const {expect} = require('chai')
+const request = require('supertest')
+const db = require('../db')
+const app = require('../index')
+const Product = db.model('product')
+
+describe('Product routes', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('/api/products/', () => {
+    const sampleProduct = {
+      name: 'cocoa puffs',
+      price: 5
+    }
+
+    beforeEach(() => {
+      return Product.create(sampleProduct)
+    })
+
+    it('GET /api/products', async () => {
+      const res = await request(app)
+        .get('/api/products')
+        .expect(200)
+
+      expect(res.body).to.be.an('array')
+      expect(res.body[0].name).to.be.equal(sampleProduct.name)
+    })
+  }) // end describe('/api/users')
+}) // end describe('User routes')
---
+++ server/db/models/products.js
@@ -0,0 +1,36 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define('product', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+    validate: {
+      min: 0
+    }
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue:
+      'https://cdn.shopify.com/s/files/1/1305/4751/products/StaySteady_Vanilla_Almond_Life_2000x.png?v=1542324908'
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+    validate: {
+      min: 0
+    }
+  }
+})
+
+module.exports = Product
