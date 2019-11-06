8f6aaab5ec6e533c520f64d84b7f83d2392d6e94
Haley Sambursky <hsambursky46@gmail.com>
2019-11-06 16:08:26 -0500

    feat: made mycart link visible when logged in

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -11,7 +11,8 @@ const Navbar = ({handleClick, isLoggedIn}) => (
       {isLoggedIn ? (
         <div>
           {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
+
+          <Link to="/cart">My Cart</Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
@@ -19,6 +20,7 @@ const Navbar = ({handleClick, isLoggedIn}) => (
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
+
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
           <Link to="/cart">My Cart</Link>
