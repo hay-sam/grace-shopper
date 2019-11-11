6c6e3710eaee03938bebc3ed85e2457fb41eb431
Sylvana Santos <sylvana.e.santos@gmail.com>
2019-11-11 13:18:20 -0500

    test: added tests for the orders api route

--- server/db/models/order.spec.js
+++ server/db/models/order.spec.js
@@ -1,30 +1,61 @@
 const {expect} = require('chai')
 const db = require('../index')
-const User = db.model('order')
+const User = db.model('user')
+const Product = db.model('product')
+const Order = db.model('order')
 
-// describe('Order model', () => {
-//   beforeEach(() => {
-//     return db.sync({force: true})
-//   })
+describe('Order model', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
 
-//   describe('instanceMethods', () => {
-//     describe('correctPassword', () => {
-//       let cody
+  describe('Create method', () => {
+    let order
+    beforeEach(async () => {
+      let cody = await User.create({
+        email: 'cody@puppybook.com',
+        password: 'bones'
+      })
+      let sampleProduct1 = await Product.create({
+        name: 'cocoa puffs',
+        price: 199
+      })
+      let sampleProduct2 = await Product.create({
+        name: 'chex',
+        price: 50
+      })
+      order = await Order.create({
+        firstName: 'Cody',
+        lastName: 'The Pug',
+        email: 'cody@email.com',
+        phoneNumber: '123456789',
+        address: 'Doggie Treat Lane',
+        totalPrice: 5
+      })
+      await order.setUser(cody)
+      await order.addProduct(sampleProduct1, {through: {quantity: 6}})
+      await order.addProduct(sampleProduct2, {through: {quantity: 2}})
+    })
 
-//       beforeEach(async () => {
-//         cody = await User.create({
-//           email: 'cody@puppybook.com',
-//           password: 'bones'
-//         })
-//       })
+    describe('makes order', () => {
+      it('has information', () => {
+        expect(order.firstName).to.be.equal('Cody')
+        expect(order.lastName).to.be.equal('The Pug')
+        expect(order.email).to.be.equal('cody@email.com')
+        expect(order.phoneNumber).to.be.equal('123456789')
+        expect(order.address).to.be.equal('Doggie Treat Lane')
+        expect(order.totalPrice).to.be.equal(5)
+      })
 
-//       it('returns true if the password is correct', () => {
-//         expect(cody.correctPassword('bones')).to.be.equal(true)
-//       })
+      it('is associated with user', () => {
+        expect(order.userId).to.be.equal(1)
+      })
 
-//       it('returns false if the password is incorrect', () => {
-//         expect(cody.correctPassword('bonez')).to.be.equal(false)
-//       })
-//     })
-//   })
-// })
+      it('contains products and their corresponding quantities', async () => {
+        const products = await order.getProducts()
+        expect(products[0].name).to.be.equal('cocoa puffs')
+        expect(products[0].orderProducts.quantity).to.be.equal(6)
+      })
+    })
+  })
+})
