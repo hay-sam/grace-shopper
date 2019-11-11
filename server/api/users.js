e986f2f6874471d18e2514afdbc954aef5ab6d12
Joy Wang <w.joy5015@gmail.com>
2019-11-11 15:08:02 -0500

    detailed product component created

--- server/api/users.js
+++ server/api/users.js
@@ -2,8 +2,17 @@ const router = require('express').Router()
 const {User, Order, Product} = require('../db/models')
 module.exports = router
 
+//securty check function that will stop the router request if the user is not who they say they are
+const isMe = (req, res, next) => {
+  if (Number(req.params.userId) === Number(req.user.id)) {
+    next()
+  } else {
+    res.status(403).send("Where do you think you're going?")
+  }
+}
+
 //router for when a user wants to view their own profile:
-router.get('/:id', async (req, res, next) => {
+router.get('/:id', isMe, async (req, res, next) => {
   try {
     const user = await User.findByPk(req.params.id)
     res.json(user) //returns the users as an object that contains email, password, address, etc.
@@ -13,7 +22,7 @@ router.get('/:id', async (req, res, next) => {
 })
 
 //router for when a user wants to edit their info (address/phone, etc.)
-router.put('/edit-profile/:id', async (req, res, next) => {
+router.put('/edit-profile/:id', isMe, async (req, res, next) => {
   try {
     const user = await User.findByPk(req.params.id)
     const newProperties = {
@@ -28,14 +37,6 @@ router.put('/edit-profile/:id', async (req, res, next) => {
     next(err)
   }
 })
-
-const isMe = (req, res, next) => {
-  if (Number(req.params.userId) === Number(req.user.id)) {
-    next()
-  } else {
-    res.status(403).send("Where do you think you're going?")
-  }
-}
 
 router.get('/:userId/orders', isMe, async (req, res, next) => {
   try {
