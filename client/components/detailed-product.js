7136e8041fe4e50d15124f50162527e4f1048b9c
Joy Wang <w.joy5015@gmail.com>
2019-11-12 14:51:59 -0500

    feat: added styling to detailed-products view and user-profile view

--- client/components/detailed-product.js
+++ client/components/detailed-product.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import {addToCart} from '../store/cart'
 import {singleProductThunk} from '../store/detailed-product'
 import convertToDollars from '../../utils/utils'
+import {toast} from 'react-toastify'
 
 class DetailedProduct extends React.Component {
   constructor(props) {
@@ -20,22 +21,28 @@ class DetailedProduct extends React.Component {
     event.preventDefault()
     let product = this.props.product
     this.props.addToCart({product: product, quantity: 1})
+    toast.success(`${product.name} added to cart!`)
   }
 
   render() {
     const {product} = this.props
 
     return (
-      <div>
-        <span className="single-product-name">{product.name}</span>
+      <div className="detailed-product-container">
         <img className="single-product-img" src={product.imageUrl} />
-        <span className="single-product-price">
-          Price: {convertToDollars(product.price)}
-        </span>
-        <p>{product.description}</p>
-        <button onClick={() => this.handleClick()} className="add-to-cart">
-          Add To Cart
-        </button>
+        <div className="single-product-info">
+          <h3 className="single-product-name">{product.name}</h3>
+          <p className="single-product-price">
+            Price: {convertToDollars(product.price)}
+          </p>
+          <p>{product.description}</p>
+          <button
+            onClick={event => this.handleClick(event)}
+            className="add-to-cart"
+          >
+            Add To Cart
+          </button>
+        </div>
       </div>
     )
   }
