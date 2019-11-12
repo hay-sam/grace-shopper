b140b2e94d8502cfa0b0c5a2cd8727accbd0b1ea
Joy Wang <w.joy5015@gmail.com>
2019-11-12 11:41:02 -0500

    fix: removed commented out code in client/store/products.js

--- client/store/products.js
+++ client/store/products.js
@@ -4,7 +4,6 @@ import axios from 'axios'
  * ACTION TYPES
  */
 const GET_PRODUCTS = 'GET_PRODUCTS'
-// const SINGLE_PRODUCT = 'SINGLE_PRODUCT'
 
 /**
  * INITIAL STATE
@@ -19,10 +18,6 @@ export const gotProductsAction = productsArr => ({
   products: productsArr
 })
 
-// const singleProductAction = productObj => ({
-//   type: SINGLE_PRODUCT,
-//   product: productObj
-// })
 /**
  * THUNK CREATORS
  */
@@ -34,12 +29,6 @@ export const gotProductsThunk = () => {
   }
 }
 
-// export const singleProductThunk = productId => {
-//   return async dispatch => {
-//     const {data} = await axios.get(`api/products/${productId}`)
-//     dispatch(singleProductAction(data))
-//   }
-// }
 /**
  * REDUCER
  */
@@ -47,8 +36,6 @@ export default function(state = initialState, action) {
   switch (action.type) {
     case GET_PRODUCTS:
       return action.products
-    // case SINGLE_PRODUCT:
-    //   return action.product
     default:
       return state
   }
